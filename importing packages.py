9fe91096e0acadc287d222c3ae7968f44c5c8e9e
Shivam Singh <35419706+Shivam928@users.noreply.github.com>
2022-09-07 09:50:42 +0530

    Add files via upload
    This file can be used to keep the track of the packages imported in the application

---
+++ importing packages.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  7 09:36:08 2022
+
+@author: FactsgeniX
+"""
+
+#importing the required packages for speech input and conversion of seech into meaningful text
+import speech_recognition as sr
+
+import pyttsx3
+
+import pprint
+
+
+#importing wikipedia package for the information extraction from wiki source
+import wikipedia
+
+#importing requests package to send HTTP requests using Python
+import requests
+
+# importing thi library for parsing HTML webpage
+from bs4 import BeautifulSoup
